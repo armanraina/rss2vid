a00eeffc1e40a2c64b6e9ee72aeeff8ae091060e
arman <arman.raina@yahoo.com>
2020-08-22 12:40:10 -0700

    add mesa3d for vm building

---
+++ assets/mesa3d-20.1.6-release-mingw/perappdeploy.cmd
@@ -0,0 +1,168 @@
+@echo off
+@cd /d "%~dp0"
+@set "ERRORLEVEL="
+@CMD /C EXIT 0
+@"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@powershell -Command Start-Process ""%0"" -Verb runAs 2>nul
+@exit
+)
+:--------------------------------------
+@TITLE Mesa3D per-application deployment utility
+@echo Mesa3D per-application deployment utility
+@echo -----------------------------------------
+@echo This deployment utility allows for per-application deployments of Mesa3D
+@echo without manual copy-paste allowing updates to Mesa3D from a central location.
+@echo It is intended for systems with working GPUs.
+@echo This helps a lot if you have many programs that you want to use Mesa3D with.
+@echo Some applications may still use the GPU if they are smart enough to only load
+@echo OpenGL DLL from system directory.
+@echo To overcome this you may enter the program executable filename and a .local
+@echo file is generated for it or use Mesainjector made by Federico Dossena.
+@echo However Mesainjector does not work with Windows 10 Version 1803 and newer.
+@echo Download: http://downloads.fdossena.com/Projects/Mesa3D/Injector/index.php
+@echo Build Mesainjector - https://fdossena.com/?p=mesa/injector_build.frag
+@echo.
+@pause
+@set mesaloc=%~dp0
+@IF "%mesaloc:~-1%"=="\" set mesaloc=%mesaloc:~0,-1%
+
+:deploy
+@cls
+@echo Mesa3D per-application deployment utility
+@echo -----------------------------------------
+@echo Please provide the path to the folder that contains the application launcher
+@echo executable. It is recommended to copy-paste it from Windows Explorer using
+@echo CTRL+V. The right click paste introduced in Windows 10 may lead to unexpected
+@echo double paste. Also don't worry if path contains spaces, parantheses or other
+@echo symbols, it is enclosed in quotes automatically so you don't need to add them
+@echo manually.
+@echo.
+@set /p dir=Path to folder holding application executable:
+@IF NOT EXIST "%dir%" echo.
+@IF NOT EXIST "%dir%" echo Error: That location doesn't exist.
+@IF NOT EXIST "%dir%" pause
+@IF NOT EXIST "%dir%" GOTO deploy
+@echo.
+
+:askforappexe
+@set /p appexe=Application executable name with or without extension (optional, forces some programs to use Mesa3D which would otherwise bypass it):
+@IF "%appexe%"=="" echo.
+@IF "%appexe%"=="" GOTO ask_for_app_abi
+@IF /I NOT "%appexe:~-4%"==".exe" set appexe=%appexe%.exe
+@IF NOT EXIST "%dir%\%appexe%" echo.
+@IF NOT EXIST "%dir%\%appexe%" echo Error: File not found.
+@IF NOT EXIST "%dir%\%appexe%" pause
+@IF NOT EXIST "%dir%\%appexe%" cls
+@IF NOT EXIST "%dir%\%appexe%" GOTO askforappexe
+
+:ask_for_app_abi
+@set mesadll=x86
+@if /I NOT %PROCESSOR_ARCHITECTURE%==AMD64 GOTO desktopgl
+@set /p ABI=This is a 64-bit application (y=yes):
+@if /I "%ABI%"=="y" set mesadll=x64
+@echo.
+
+:desktopgl
+@set desktopgl=y
+@set /p desktopgl=Do you want Desktop OpenGL drivers (y/n, defaults to yes):
+@echo.
+@IF /I "%desktopgl%"=="n" GOTO opengles
+@IF EXIST "%dir%\opengl32.dll" echo Updated softpipe and llvmpipe deployment.
+@IF EXIST "%dir%\opengl32.dll" del "%dir%\opengl32.dll"
+@IF EXIST "%dir%\libglapi.dll" del "%dir%\libglapi.dll"
+@mklink "%dir%\opengl32.dll" "%mesaloc%\%mesadll%\opengl32.dll"
+@mklink "%dir%\libglapi.dll" "%mesaloc%\%mesadll%\libglapi.dll"
+@IF NOT "%dir%\%appexe%"=="%dir%\" echo dummy > "%dir%\%appexe%.local"
+@echo.
+@set swr=n
+@if NOT %mesadll%==x64 GOTO opengles
+@IF EXIST "%mesaloc%\%mesadll%\swr*.dll" set /p swr=Do you want swr driver - the new desktop OpenGL driver made by Intel (y/n):
+@IF EXIST "%mesaloc%\%mesadll%\swr*.dll" echo.
+@IF /I NOT "%swr%"=="y" GOTO opengles
+@if EXIST "%dir%\swrAVX.dll" (
+@echo Updated swr driver deployment.
+@GOTO deployswr
+)
+@if EXIST "%dir%\swrAVX2.dll" (
+@echo Updated swr driver deployment.
+@GOTO deployswr
+)
+@if EXIST "%dir%\swrSKX.dll" (
+@echo Updated swr driver deployment.
+@GOTO deployswr
+)
+@if EXIST "%dir%\swrKNL.dll" (
+@echo Updated swr driver deployment.
+@GOTO deployswr
+)
+
+:deployswr
+@if EXIST "%dir%\swrAVX.dll" del "%dir%\swrAVX.dll"
+@if EXIST "%dir%\swrAVX2.dll" del "%dir%\swrAVX2.dll"
+@if EXIST "%dir%\swrSKX.dll" del "%dir%\swrSKX.dll"
+@if EXIST "%dir%\swrKNL.dll" del "%dir%\swrKNL.dll"
+@mklink "%dir%\swrAVX.dll" "%mesaloc%\x64\swrAVX.dll"
+@mklink "%dir%\swrAVX2.dll" "%mesaloc%\x64\swrAVX2.dll"
+@IF EXIST "%mesaloc%\x64\swrSKX.dll" mklink "%dir%\swrSKX.dll" "%mesaloc%\x64\swrSKX.dll"
+@IF EXIST "%mesaloc%\x64\swrKNL.dll" mklink "%dir%\swrKNL.dll" "%mesaloc%\x64\swrKNL.dll"
+@echo.
+
+:opengles
+@IF EXIST "%mesaloc%\%mesadll%\libglapi.dll" set /p opengles=Do you need OpenGL ES support (y/n):
+@IF /I NOT "%opengles%"=="y" GOTO osmesa
+@IF EXIST "%dir%\libglapi.dll" del "%dir%\libglapi.dll"
+@if EXIST "%dir%\libGLESv1_CM.dll" del "%dir%\libGLESv1_CM.dll"
+@if EXIST "%dir%\libGLESv2.dll" del "%dir%\libGLESv2.dll"
+@mklink "%dir%\libglapi.dll" "%mesaloc%\%mesadll%\libglapi.dll"
+@IF EXIST "%mesaloc%\%mesadll%\libGLESv1_CM.dll" mklink "%dir%\libGLESv1_CM.dll" "%mesaloc%\%mesadll%\libGLESv1_CM.dll"
+@IF EXIST "%mesaloc%\%mesadll%\libGLESv2.dll" mklink "%dir%\libGLESv2.dll" "%mesaloc%\%mesadll%\libGLESv2.dll"
+@echo.
+
+:osmesa
+@set osmesatype=n
+@set /p osmesa=Do you need off-screen rendering (y/n):
+@echo.
+@if /I NOT "%osmesa%"=="y" GOTO graw
+@IF EXIST "%dir%\osmesa.dll" echo Updated Mesa3D off-screen rendering interface deployment.
+@IF EXIST "%dir%\osmesa.dll" del "%dir%\osmesa.dll"
+@IF EXIST "%dir%\libglapi.dll" del "%dir%\libglapi.dll"
+@IF EXIST "%mesaloc%\%mesadll%\osmesa.dll" mklink "%dir%\osmesa.dll" "%mesaloc%\%mesadll%\osmesa.dll"
+@IF EXIST "%mesaloc%\%mesadll%\libglapi.dll" mklink "%dir%\libglapi.dll" "%mesaloc%\%mesadll%\libglapi.dll"
+@echo.
+@IF EXIST "%mesaloc%\%mesadll%\osmesa.dll" GOTO graw
+@IF EXIST "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll" IF EXIST "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll" echo What version of osmesa off-screen rendering you want:
+@IF EXIST "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll" IF EXIST "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll" echo 1. Gallium based (faster, but lacks certain features)
+@IF EXIST "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll" IF EXIST "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll" echo 2. Swrast based (slower, but has unique OpenGL 2.1 features)
+@IF EXIST "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll" IF EXIST "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll" set /p osmesatype=Enter choice:
+@IF EXIST "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll" IF EXIST "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll" echo.
+@if "%osmesatype%"=="1" mklink "%dir%\osmesa.dll" "%mesaloc%\%mesadll%\osmesa-gallium\osmesa.dll"
+@if "%osmesatype%"=="2" mklink "%dir%\osmesa.dll" "%mesaloc%\%mesadll%\osmesa-swrast\osmesa.dll"
+@if "%osmesatype%"=="1" echo.
+@if "%osmesatype%"=="2" echo.
+
+:graw
+@set /p graw=Do you need graw library (y/n):
+@echo.
+@if /I NOT "%graw%"=="y" GOTO restart
+@if EXIST "%dir%\graw.dll" (
+@echo Updated Mesa3D graw framework deployment.
+@GOTO deploygraw
+)
+@if EXIST "%dir%\graw_null.dll" (
+@echo Updated Mesa3D graw framework deployment.
+@GOTO deploygraw
+)
+
+:deploygraw
+@if EXIST "%dir%\graw.dll" del "%dir%\graw.dll"
+@if EXIST "%dir%\graw_null.dll" del "%dir%\graw_null.dll"
+@IF EXIST "%dir%\libglapi.dll" del "%dir%\libglapi.dll"
+@IF EXIST "%mesaloc%\%mesadll%\graw.dll" mklink "%dir%\graw.dll" "%mesaloc%\%mesadll%\graw.dll"
+@IF EXIST "%mesaloc%\%mesadll%\graw_null.dll" mklink "%dir%\graw_null.dll" "%mesaloc%\%mesadll%\graw_null.dll"
+@IF EXIST "%mesaloc%\%mesadll%\libglapi.dll" mklink "%dir%\libglapi.dll" "%mesaloc%\%mesadll%\libglapi.dll"
+@echo.
+
+:restart
+@set /p rerun=More Mesa deployment? (y=yes):
+@if /I "%rerun%"=="y" GOTO deploy
---
+++ assets/mesa3d-20.1.6-release-mingw/systemwidedeploy.cmd
@@ -0,0 +1,189 @@
+@echo off
+@cd /d "%~dp0"
+@set "ERRORLEVEL="
+@CMD /C EXIT 0
+@"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@powershell -Command Start-Process ""%0"" -Verb runAs 2>nul
+@exit
+)
+:--------------------------------------
+@TITLE Mesa3D system-wide deployment utility
+@echo Mesa3D system-wide deployment utility
+@echo -------------------------------------
+@echo This deployment utility targets systems without working GPUs and any use case
+@echo where hardware accelerated OpenGL is not available. This mainly covers
+@echo virtual machines in cloud environments and RDP connections. It can be
+@echo used to replace Microsoft Windows inbox OpenGL 1.1 software render
+@echo driver with Mesa3D softpipe, llvmpipe or swr driver.
+@echo.
+@pause
+@set mesaloc=%~dp0
+@IF "%mesaloc:~-1%"=="\" set mesaloc=%mesaloc:~0,-1%
+
+:deploy
+@cls
+@set mesainstalled=1
+@IF NOT EXIST "%windir%\System32\mesadrv.dll" IF NOT EXIST "%windir%\System32\graw.dll" IF NOT EXIST "%windir%\System32\osmesa.dll" set mesainstalled=0
+
+@echo Mesa3D system-wide deployment utility
+@echo -------------------------------------
+@echo Please make a deployment choice:
+@echo 1. Desktop OpenGL drivers (softpipe and llvmpipe only);
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\swr*.dll" echo 2. Desktop OpenGL drivers (softpipe, llvmpipe and swr drivers);
+@echo 3. Mesa3D off-screen render driver gallium version (osmesa gallium);
+@IF NOT EXIST "%mesaloc%\x86\osmesa.dll" IF NOT EXIST "%mesaloc%\x64\osmesa.dll" echo 4. Mesa3D off-screen render driver classic version (osmesa swrast);
+@echo 5. Mesa3D graw test framework;
+@IF %mesainstalled%==1 echo 6. Update system-wide deployment
+@IF %mesainstalled%==1 echo 7. Remove system-wide deployments (uninstall);
+@IF %mesainstalled%==1 echo 8. Exit
+@IF %mesainstalled%==0 echo 6. Exit
+@set /p deploychoice=Enter choice:
+@if "%deploychoice%"=="1" GOTO desktopgl
+@if "%deploychoice%"=="2" GOTO desktopgl
+@if "%deploychoice%"=="3" GOTO osmesa
+@if "%deploychoice%"=="4" GOTO osmesa
+@if "%deploychoice%"=="5" GOTO graw
+@if "%deploychoice%"=="6" IF %mesainstalled%==1 GOTO update
+@if "%deploychoice%"=="7" IF %mesainstalled%==1 GOTO uninstall
+@if "%deploychoice%"=="8" IF %mesainstalled%==1 GOTO exit
+@if "%deploychoice%"=="6" IF %mesainstalled%==0 GOTO exit
+@echo Invaild entry
+@pause
+@GOTO deploy
+
+:desktopgl
+@if "%deploychoice%"=="2" if /I NOT %PROCESSOR_ARCHITECTURE%==AMD64 echo Invalid choice. swr driver is only supported on X64/AMD64 systems.
+@if "%deploychoice%"=="2" if /I NOT %PROCESSOR_ARCHITECTURE%==AMD64 pause
+@if "%deploychoice%"=="2" if /I NOT %PROCESSOR_ARCHITECTURE%==AMD64 GOTO deploy
+@if "%deploychoice%"=="2" if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF NOT EXIST "%mesaloc%\x64\swr*.dll" echo Invalid choice. swr driver is not included in MSYS2 Mingw-w64 build of Mesa3D.
+@if "%deploychoice%"=="2" if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF NOT EXIST "%mesaloc%\x64\swr*.dll" pause
+@if "%deploychoice%"=="2" if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF NOT EXIST "%mesaloc%\x64\swr*.dll" GOTO deploy
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 copy "%mesaloc%\x86\opengl32.dll" "%windir%\System32\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x86\opengl32.dll" "%windir%\SysWOW64\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x64\opengl32.dll" "%windir%\System32\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\libglapi.dll" copy "%mesaloc%\x64\libglapi.dll" "%windir%\System32"
+@if "%deploychoice%"=="2" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x64\swr*.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "DLL" /t REG_SZ /d "mesadrv.dll" /f
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "DriverVersion" /t REG_DWORD /d "1" /f
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "Flags" /t REG_DWORD /d "1" /f
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "Version" /t REG_DWORD /d "2" /f
+@REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "DLL" /t REG_SZ /d "mesadrv.dll" /f
+@REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "DriverVersion" /t REG_DWORD /d "1" /f
+@REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "Flags" /t REG_DWORD /d "1" /f
+@REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /v "Version" /t REG_DWORD /d "2" /f
+@echo.
+@echo Desktop OpenGL drivers deploy complete.
+@pause
+@GOTO deploy
+
+:osmesa
+@if "%deploychoice%"=="3" IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%mesaloc%\x86\osmesa.dll" copy "%mesaloc%\x86\osmesa.dll" "%windir%\System32"
+@if "%deploychoice%"=="3" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x86\osmesa.dll" copy "%mesaloc%\x86\osmesa.dll" "%windir%\SysWOW64"
+@if "%deploychoice%"=="3" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\osmesa.dll" copy "%mesaloc%\x64\osmesa.dll" "%windir%\System32"
+@if "%deploychoice%"=="4" IF EXIST %mesaloc%\x86\osmesa.dll IF EXIST %mesaloc%\x64\osmesa.dll echo Mesa3D was built with Meson so osmesa swrast is not available.
+@if "%deploychoice%"=="4" IF EXIST %mesaloc%\x86\osmesa.dll IF EXIST %mesaloc%\x64\osmesa.dll pause
+@if "%deploychoice%"=="4" IF EXIST %mesaloc%\x86\osmesa.dll IF EXIST %mesaloc%\x64\osmesa.dll GOTO deploy
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\libglapi.dll" copy "%mesaloc%\x64\libglapi.dll" "%windir%\System32"
+@if "%deploychoice%"=="3" set osmesatype=gallium
+@if "%deploychoice%"=="4" set osmesatype=swrast
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 copy "%mesaloc%\x86\osmesa-%osmesatype%\osmesa.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x86\osmesa-%osmesatype%\osmesa.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x64\osmesa-%osmesatype%\osmesa.dll" "%windir%\System32"
+@echo.
+@echo Off-screen render driver deploy complete.
+@pause
+@GOTO deploy
+
+:graw
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 copy "%mesaloc%\x86\graw.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x86\graw.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 copy "%mesaloc%\x64\graw.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%mesaloc%\x86\graw_null.dll" copy "%mesaloc%\x86\graw_null.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x86\graw_null.dll" copy "%mesaloc%\x86\graw_null.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\graw_null.dll" copy "%mesaloc%\x64\graw_null.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%mesaloc%\x64\libglapi.dll" copy "%mesaloc%\x64\libglapi.dll" "%windir%\System32"
+@echo.
+@echo graw framework deploy complete.
+@pause
+@GOTO deploy
+
+:update
+@IF %mesainstalled%==0 echo.
+@IF %mesainstalled%==0 echo Error: No Mesa3D drivers installed.
+@IF %mesainstalled%==0 pause
+@IF %mesainstalled%==0 GOTO deploy
+
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\mesadrv.dll" copy "%mesaloc%\x86\opengl32.dll" "%windir%\System32\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\mesadrv.dll" copy "%mesaloc%\x86\opengl32.dll" "%windir%\SysWOW64\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\mesadrv.dll" copy "%mesaloc%\x64\opengl32.dll" "%windir%\System32\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\libglapi.dll" IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\libglapi.dll" IF EXIST "%mesaloc%\x86\libglapi.dll" copy "%mesaloc%\x86\libglapi.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\libglapi.dll" IF EXIST "%mesaloc%\x64\libglapi.dll" copy "%mesaloc%\x64\libglapi.dll" "%windir%\System32"
+@IF EXIST "%windir%\System32\swrAVX.dll" copy "%mesaloc%\x64\swrAVX.dll" "%windir%\System32"
+@IF EXIST "%windir%\System32\swrAVX2.dll" copy "%mesaloc%\x64\swrAVX2.dll" "%windir%\System32"
+@IF EXIST "%windir%\System32\swrSKX.dll" copy "%mesaloc%\x64\swrSKX.dll" "%windir%\System32"
+@IF EXIST "%windir%\System32\swrKNL.dll" copy "%mesaloc%\x64\swrKNL.dll" "%windir%\System32"
+
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\graw.dll" copy "%mesaloc%\x86\graw.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\graw.dll" copy "%mesaloc%\x86\graw.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\graw.dll" copy "%mesaloc%\x64\graw.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\graw.dll" IF EXIST "%mesaloc%\x86\graw_null.dll" copy "%mesaloc%\x86\graw_null.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\graw.dll" IF EXIST "%mesaloc%\x86\graw_null.dll" copy "%mesaloc%\x86\graw_null.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\graw.dll" IF EXIST "%mesaloc%\x64\graw_null.dll" copy "%mesaloc%\x64\graw_null.dll" "%windir%\System32"
+
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\osmesa.dll" IF EXIST "%mesaloc%\x86\osmesa.dll" copy "%mesaloc%\x86\osmesa.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\osmesa.dll" IF EXIST "%mesaloc%\x86\osmesa.dll" copy "%mesaloc%\x86\osmesa.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\osmesa.dll" IF EXIST "%mesaloc%\x64\osmesa.dll" copy "%mesaloc%\x64\osmesa.dll" "%windir%\System32"
+@IF EXIST "%windir%\System32\osmesa.dll" IF EXIST "%mesaloc%\x86\osmesa.dll" IF EXIST "%mesaloc%\x64\osmesa.dll" GOTO doneupdate
+@set BYTES=10000000
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\osmesa.dll" for %%f in ("%windir%\System32\osmesa.dll") do @set BYTES=%%~zf
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\osmesa.dll" IF %BYTES% GTR 10000000 copy "%mesaloc%\x86\osmesa-gallium\osmesa.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==X86 IF EXIST "%windir%\System32\osmesa.dll" IF %BYTES% LSS 10000000 copy "%mesaloc%\x86\osmesa-swrast\osmesa.dll" "%windir%\System32"
+@set BYTES=10000000
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\osmesa.dll" for %%f in ("%windir%\SysWOW64\osmesa.dll") do @set BYTES=%%~zf
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\osmesa.dll" IF %BYTES% GTR 10000000 copy "%mesaloc%\x86\osmesa-gallium\osmesa.dll" "%windir%\SysWOW64"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\osmesa.dll" IF %BYTES% LSS 10000000 copy "%mesaloc%\x86\osmesa-swrast\osmesa.dll" "%windir%\SysWOW64"
+@set BYTES=10000000
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\osmesa.dll" for %%f in ("%windir%\System32\osmesa.dll") do @set BYTES=%%~zf
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\osmesa.dll" IF %BYTES% GTR 10000000 copy "%mesaloc%\x64\osmesa-gallium\osmesa.dll" "%windir%\System32"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\System32\osmesa.dll" IF %BYTES% LSS 10000000 copy "%mesaloc%\x64\osmesa-swrast\osmesa.dll" "%windir%\System32"
+
+:doneupdate
+@echo.
+@echo Update complete.
+@pause
+@GOTO deploy
+
+:uninstall
+@REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /f
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /f
+@IF EXIST "%windir%\System32\mesadrv.dll" del "%windir%\System32\mesadrv.dll"
+@IF EXIST "%windir%\System32\libglapi.dll" del "%windir%\System32\libglapi.dll"
+@IF EXIST "%windir%\System32\graw.dll" del "%windir%\System32\graw.dll"
+@IF EXIST "%windir%\System32\graw_null.dll" del "%windir%\System32\graw_null.dll"
+@IF EXIST "%windir%\System32\osmesa.dll" del "%windir%\System32\osmesa.dll"
+@IF EXIST "%windir%\System32\swrAVX.dll" del "%windir%\System32\swrAVX.dll"
+@IF EXIST "%windir%\System32\swrAVX2.dll" del "%windir%\System32\swrAVX2.dll"
+@IF EXIST "%windir%\System32\swrSKX.dll" del "%windir%\System32\swrSKX.dll"
+@IF EXIST "%windir%\System32\swrKNL.dll" del "%windir%\System32\swrKNL.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\mesadrv.dll" del "%windir%\SysWOW64\mesadrv.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\libglapi.dll" del "%windir%\SysWOW64\libglapi.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\osmesa.dll" del "%windir%\SysWOW64\osmesa.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\graw.dll" del "%windir%\SysWOW64\graw.dll"
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%windir%\SysWOW64\graw_null.dll" del "%windir%\SysWOW64\graw_null.dll"
+@echo.
+@echo Uninstall complete.
+@pause
+@GOTO deploy
+
+:exit
+@echo Good Bye!
+@pause
+@exit
